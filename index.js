80d8a64433da128c1a283353a3fad81d97187935
sumitdevs <mysumitdevs@gmail.com>
2024-08-16 19:43:44 +0530

    basic ejs file is configured

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from "express";
 
+
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 
 app.get('/', (req, res) =>{
-    res.send("Hello world")
+    res.render('index.ejs');
 })
 
 app.listen(port, () => {
