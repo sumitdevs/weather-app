715dd3b3a64816520d216161d1235e141b64dcc2
sumitdevs <mysumitdevs@gmail.com>
2024-08-16 19:21:45 +0530

    intial file is created

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get('/', (req, res) =>{
+    res.send("Hello world")
+})
+
+app.listen(port, () => {
+    console.log(`server started on port: ${port}`);
+})
